7e1bc943e093f75a752d956c2779e06be3b2c21e
Daizman <andihell@yandex.ru>
2026-01-17 22:48:30 +0300

    Init migration

--- EFExampleApplication/Composer.cs
+++ EFExampleApplication/Composer.cs
@@ -1,5 +1,7 @@
 using EFExampleApplication.Abstractions;
+using EFExampleApplication.Database;
 using EFExampleApplication.Services;
+using Microsoft.EntityFrameworkCore;
 
 namespace EFExampleApplication;
 
@@ -8,6 +10,12 @@ public static class Composer
     public static IServiceCollection AddInfrastructure(this IServiceCollection services)
     {
         services.AddAutoMapper(typeof(Composer).Assembly);
+        services.AddDbContext<ApplicationDbContext>(options =>
+        {
+            options.UseNpgsql(
+                "Host=localhost;Port=5432;Username=postgres;Password=postgres;Database=EFExampleApplication"
+            );
+        });
         services.AddExceptionHandler<ExceptionHandler>();
         services.AddControllers();
 
